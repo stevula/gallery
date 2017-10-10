a524dd5e867982940d522b4f38d92bb2a7e8d031
stevula <brodericksteven@gmail.com>
2017-10-09 22:47:32 -0700

    Add prop-types package

--- src/GalleryImage/index.js
+++ src/GalleryImage/index.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class GalleryImage extends Component {
   render() {
     const style = {
-      width: '33%',
+      width: '33.33%',
     };
 
     return (
       <img style={style} src={this.props.src} alt="" />
     );
   }
+}
+
+GalleryImage.propTypes = {
+  src: PropTypes.string.isRequired,
 }
 
 export default GalleryImage;
